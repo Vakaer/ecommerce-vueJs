127a594b32a7dd42cb72dab4a6678bfb43429e95
Vakaer <106831146+Vakaer@users.noreply.github.com>
2023-10-13 21:03:55 +0500

    added filtered products action

--- src/store/ProductStore.js
+++ src/store/ProductStore.js
@@ -1,10 +1,10 @@
-import { getAllProducts } from "@/services/productService";
+import { getAllProducts, getProductsByCategory } from "@/services/productService";
 import { defineStore } from "pinia";
 
 export const useProductStore = defineStore("productStore", {
 	state: () => ({
+		productByCategory: [],
 		products: [],
-		categories:[],
 		error: null,
 		loading: false, // Add an error property to store error information
 	}),
@@ -14,30 +14,40 @@ export const useProductStore = defineStore("productStore", {
 			this.loading = true;
 			try {
 				const response = await getAllProducts();
-				if (response.status === 200) {
-					this.products = response.data;
+				console.log("response", response);
+				if (response) {
+					this.products = response;
 					this.error = null; // Reset the error if the request was successful
 				} else {
 					this.error = "Server returned a non-200 status code"; // Handle non-200 status codes
 				}
 			} catch (error) {
 				this.error = "An error occurred while fetching products"; // Handle network or other errors
-				console.error("Error fetching products:", error);
 			} finally {
 				this.loading = false;
 			}
 		},
-		
-	},
-
-	getters: {
-		// This getter will compute and return an array of unique categories
-		uniqueCategories() {
-		  const categories = new Set();
-		  for (const product of this.products) {
-			categories.add(product.category);
-		  }
-		  return Array.from(categories);
+		async addProduct(data) {
+			try {
+				const response = await this.addProduct(data);
+			} catch (error) {}
 		},
-	  },
+		async getCategorySpecificProducts(category) {
+			debugger
+			this.loading = true;
+			try {
+				const response = await getProductsByCategory(category);
+				if (response) {
+					this.productByCategory = response.data;
+					this.error = null; // Reset the error if the request was successful
+				} else {
+					this.error = "Server returned a non-200 status code"; // Handle non-200 status codes
+				}
+			} catch (error) {
+				this.error = "An error occurred while filtering products by category"; // Handle network or other errors
+			} finally {
+				this.loading = false;
+			}
+		},
+	},
 });
