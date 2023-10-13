8c3371c3252db85d3573b4f7db0b2b4431aae94a
Vakaer <106831146+Vakaer@users.noreply.github.com>
2023-10-13 17:13:19 +0500

    added intercept layout in auth layout

--- src/Layouts/AuthLayout.vue
+++ src/Layouts/AuthLayout.vue
@@ -1,9 +1,43 @@
 <template>
-    <slot></slot>
-</template>
-
-<script>
-export default {
+    <div>
+      <TheTopHeader />
+      <TheMainHeader />
+      <dialog></dialog>
+      <slot></slot>
+      <TheFooter />
+    </div>
+  </template>
     
-}
-</script>
+    
+  <script>
+  import TheMainHeader from "@/components/TheMainHeader.vue";
+  import TheFooter from "@/components/TheFooter.vue";
+  import TheTopHeader from "@/components/TheTopHeader.vue";
+  import SearchBar from "@/components/SearchBar.vue";
+  import HomeBanner from "@/components/HomeBanner.vue";
+  import SideDropDownMenu from "@/components/SideDropDownMenu.vue";
+  export default {
+    name: 'Intercept',
+    components: {
+      TheMainHeader,
+      TheTopHeader,
+      TheFooter,
+      SearchBar,
+      HomeBanner,
+      SideDropDownMenu,
+    },
+    data() {
+      return {
+        msg: "",
+        type: "",
+        modal: false,
+        loading: false,
+      };
+    },
+  };
+  </script>
+  
+  
+  
+  
+    
