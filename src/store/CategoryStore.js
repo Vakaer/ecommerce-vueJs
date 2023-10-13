dec1c0d5655a84a59353e5d5fba95c2d0a9656b6
Vakaer <106831146+Vakaer@users.noreply.github.com>
2023-10-13 21:03:00 +0500

    added category store

---
+++ src/store/CategoryStore.js
@@ -0,0 +1,32 @@
+import { getAllCategories } from "@/services/CategoryService";
+import { defineStore } from "pinia";
+
+
+export const useCategoryStore = defineStore("categoryStore", {
+	state: () => ({
+		categories: [],
+		error: null,
+		loading: false, // Add an error property to store error information
+	}),
+
+	actions: {
+		async getCategories() {
+			this.loading = true;
+			try {
+				const response = await getAllCategories();
+				console.log("response", response);
+				if (response) {
+					this.categories = response.data;
+					this.error = null; // Reset the error if the request was successful
+				} else {
+					this.error = "Server returned a non-200 status code"; // Handle non-200 status codes
+				}
+			} catch (error) {
+				this.error = "An error occurred while fetching categories"; // Handle network or other errors
+				
+			} finally {
+				this.loading = false;
+			}
+		},
+	},
+});
