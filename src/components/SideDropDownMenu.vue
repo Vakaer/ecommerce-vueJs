45d57eec096448236d69eca441a9dfc649a97ace
Vakaer <106831146+Vakaer@users.noreply.github.com>
2023-10-13 21:01:42 +0500

    dynamic categories

--- src/components/SideDropDownMenu.vue
+++ src/components/SideDropDownMenu.vue
@@ -2,36 +2,30 @@
     <!-- component -->
 
     <div class="w-width-24">
-        <div class="bg-primary py-4 flex justify-center items-center gap-3" @click="toggleContent">
+        <div class="bg-primary py-4 flex justify-center items-center gap-3 cursor-pointer" @click="toggleContent">
             <span class="text-white font-bold">Products Category</span>
-            <font-awesome-icon :icon="['fas', 'bars']" size="lg"  />
-            <font-awesome-icon :icon="['fas', 'caret-down']" size="lg" color="#fff" :class="[`${showContent ? 'rotate-360' : 'rotate-180'}`]" />
+            <font-awesome-icon :icon="['fas', 'caret-down']" size="lg" color="#fff"
+                :class="[`${showContent ? 'rotate-360' : 'rotate-180'}`]" />
         </div>
         <div class="ps-6 border transition ease-out duration-1000" :class="{ 'h-0': !showContent }" v-if="showContent">
-            <ul class="flex flex-col py-5 gap-3" v-for="category in categories" :key="category" >
+            <ul class="flex flex-col py-5 gap-3" v-for="category in categories" :key="category">
                 <li>{{ category }}</li>
-                <!-- <li>A second item</li>
-                <li>A third item</li>
-                <li>A fourth item</li>
-                <li>And a fifth one</li>
-                <li>An item</li>
-                <li>A second item</li>
-                <li>A third item</li>
-                <li>A fourth item</li>
-                <li>And a fifth one</li> -->
             </ul>
         </div>
     </div>
 </template>
 
 <script>
-import { useProductStore } from '@/store/ProductStore';
+import { useCategoryStore } from '@/store/CategoryStore';
+import { storeToRefs } from 'pinia';
 
 export default {
     name: "SideDropDownMenu",
-    setup(){
-      const productStore = useProductStore(); 
-      return{productStore}
+    setup() {
+        const categoryStore = useCategoryStore();
+        const { getCategories } = categoryStore;
+        const { categories } = storeToRefs(categoryStore);
+        return { categories, getCategories };
     },
     data() {
         return {
@@ -42,12 +36,10 @@ export default {
         toggleContent() {
             this.showContent = !this.showContent;
         },
-        
     },
-    computed:{
-        categories(){
-            this.productStore.uniqueCategories
-        }
+    created() {
+        this.getCategories();
     }
+
 };
 </script>
