87519eff4ca89fc725cf1aaf9829627ea941c22a
Vakaer <106831146+Vakaer@users.noreply.github.com>
2023-10-16 21:24:40 +0500

    login/logout button and user email show on auth

--- src/components/TheTopHeader.vue
+++ src/components/TheTopHeader.vue
@@ -1,40 +1,62 @@
 <template>
-   <!-- Main navigation container -->
-<!-- Main navigation container -->
-<nav
-  class="relative flex w-full flex-wrap items-center justify-between bg-[#f5f5f5] text-neutral-500  hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 py-1"
-  data-te-navbar-ref>
-  <div class="flex w-full flex-wrap items-center  px-3">
-    <div class="relative flex items-center w-width-60 justify-between mx-auto">
-      <div class="flex items-center">
-        <span class="flex items-center pr-3">
-          <i class="material-icons pr-2">mail</i> abc@gmail.com
-        </span>
-  
-      <!-- Twitter Icon -->
-      <span>
-      | Free shipping for all orders of $99
-      </span>
-      </div>
-      <!-- Cart Icon -->
-      
+  <!-- Main navigation container -->
+  <!-- Main navigation container -->
+  <nav
+    class="relative flex w-full flex-wrap items-center justify-between bg-[#f5f5f5] text-neutral-500  hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 py-1"
+    data-te-navbar-ref>
+    <div class="flex w-full flex-wrap items-center  px-3">
+      <div class="relative flex items-center w-width-60 justify-between mx-auto">
+        <div class="flex items-center">
+          <span v-if="authStore.isUserLoggedIn" class="flex items-center pr-3">
+            <i class="material-icons pr-2">mail</i> {{ authStore.user.email }}
+          </span>
 
-      <!-- Dropdown -->
-      <button @click="redirectToLogin" >
-        Login
-      </button>
+          <!-- Twitter Icon -->
+          <span>
+            | Free shipping for all orders of $99
+          </span>
+        </div>
+        <!-- Cart Icon -->
+
+
+        <!-- Dropdown -->
+        <button @click="handleAuthAction">
+          {{ authStore.isUserLoggedIn ? 'Logout' : 'Login' }}
+        </button>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </template>
 
 <script>
-    export default {
-        name:"TheTopHeader",
-        methods:{
-          redirectToLogin(){
-            this.$router.push({path:"/login"})
-          }
-        }
+import { useAuthStore } from '@/store/AuthStore';
+
+export default {
+  name: "TheTopHeader",
+  setup() {
+    const authStore = useAuthStore();
+    return { authStore }
+  },
+  methods: {
+    redirectToLogin() {
+      this.$router.push({ path: "/login" })
+    },
+    logout() {
+      this.authStore.logout();
+      this.redirectToLogin();
+    },
+    handleAuthAction() {
+      if (this.authStore.isUserLoggedIn) { // Access the computed property using .value
+        this.authStore.logout();
+      } else {
+        // Redirect to the login page or perform login action
+ 
+        this.$router.push({ path: "login" });
+      }
+    },
+    beforeCreate() {
+      console.log("this.authStore.isUserLoggedIn", this.authStore.isUserLoggedIn)
     }
+  }
+}
 </script>
