9d9d718139203d3c5faeb1557ad31e088eacd549
Vakaer <106831146+Vakaer@users.noreply.github.com>
2023-10-16 21:21:34 +0500

    text on home banner

--- src/components/HomeBanner.vue
+++ src/components/HomeBanner.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="bg-banner bg-cover bg-center  bg-primary" style="width: 100%; height: 400px;">
-        
+    <div class="bg-banner flex bg-cover bg-center  bg-primary" style="width: 100%; height: 400px;">
+        <div class="flex justify-center  items-start gap-4 ms-5 flex-col">
+            <h1  class="font-bold text-f44 leading-13">Best Clothing <br> Collection</h1>
+            <button class="bg-primary text-white px-4 py-2">Shop Now</button>
+        </div>
     </div>
 </template>
 
