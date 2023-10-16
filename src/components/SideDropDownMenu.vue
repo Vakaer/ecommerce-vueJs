a4cafe9184ff4057485cf776b8403bb05f59fb3a
Vakaer <106831146+Vakaer@users.noreply.github.com>
2023-10-16 21:22:43 +0500

    side menu zIndex and absolute position

--- src/components/SideDropDownMenu.vue
+++ src/components/SideDropDownMenu.vue
@@ -1,15 +1,18 @@
 <template>
     <!-- component -->
 
-    <div class="w-width-24">
-        <div class="bg-primary py-4 flex justify-center items-center gap-3 cursor-pointer" @click="toggleContent">
-            <span class="text-white font-bold">Products Category</span>
-            <font-awesome-icon :icon="['fas', 'caret-down']" size="lg" color="#fff"
-                :class="[`${showContent ? 'rotate-360' : 'rotate-180'}`]" />
-        </div>
-        <div class="ps-6 border transition ease-out duration-1000" :class="{ 'h-0': !showContent }" v-if="showContent">
-            <ul class="flex flex-col py-5 gap-3" v-for="category in categories" :key="category">
-                <li>{{ category }}</li>
+    <div class="w-width-24 ">
+        <div class="absolute w-[15%]">
+            <div class="bg-primary py-4 flex justify-center items-center gap-3 cursor-pointer" @click="toggleContent">
+                <span class="text-white font-bold">Products Category</span>
+                <font-awesome-icon :icon="['fas', 'caret-down']" size="lg" color="#fff"
+                    :class="[`${showContent ? 'rotate-360' : 'rotate-180'}`]" />
+            </div>
+            <ul class="ps-6 border transition ease-out p-3 bg-white duration-1000" :class="{ 'h-0': !showContent }"
+                v-if="showContent">
+                <li class="flex flex-col mt-2 cursor-pointer" v-for="category in categories" :key="category">
+                    <a>{{ category }}</a>
+                </li>
             </ul>
         </div>
     </div>
