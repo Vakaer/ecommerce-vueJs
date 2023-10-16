06dea3370542021d41311dcd5ce57f36d72fb860
Vakaer <106831146+Vakaer@users.noreply.github.com>
2023-10-16 21:21:57 +0500

    cart item list component

---
+++ src/components/cart/CartItemList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div v-for="item in cartStore.cartItems" :key="item.id">
+            <CartItem :cartItem="item" />
+        </div>
+    </div>
+</template>
+
+<script>
+import { useCartStore } from '@/store/CartStore';
+import CartItem from './CartItem.vue';
+
+export default {
+    components: { CartItem },
+    setup() {
+        const cartStore = useCartStore();
+        return { cartStore };
+    }
+}
+</script>
