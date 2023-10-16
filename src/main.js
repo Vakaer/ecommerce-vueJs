6d435ff10b82e640ae1f1b9aac7feaa45f0303b1
Vakaer <106831146+Vakaer@users.noreply.github.com>
2023-10-16 21:26:21 +0500

    one variable for app instance

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Vue3Toasity from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
 import "./assets/main.css";
 import { createPinia } from "pinia";
 
@@ -11,12 +13,19 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 /* import specific icons */
-import { faCartShopping, faHeart, faPhone, faHamburger, faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import {
+	faCartShopping,
+	faHeart,
+	faPhone,
+	faHamburger,
+	faCaretDown,
+} from "@fortawesome/free-solid-svg-icons";
 /* add icons to the library */
 library.add(faCartShopping, faHeart, faPhone, faHamburger, faCaretDown);
 
-createApp(App)
-	.use(createPinia())
-	.component("font-awesome-icon", FontAwesomeIcon)
-	.use(router)
-	.mount("#app");
+const app = createApp(App);
+app.use(createPinia());
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.use(Vue3Toasity);
+app.use(router);
+app.mount("#app");
