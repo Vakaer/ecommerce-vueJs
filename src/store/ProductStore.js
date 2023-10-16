dd6a8e48d18f19f0c9594a06176bebdd1c97e174
Vakaer <106831146+Vakaer@users.noreply.github.com>
2023-10-16 21:30:27 +0500

    get product by id action

--- src/store/ProductStore.js
+++ src/store/ProductStore.js
@@ -1,9 +1,10 @@
-import { getAllProducts, getProductsByCategory } from "@/services/productService";
+import { getAllProducts, getProductsByCategory, getProductsById } from "@/services/productService";
 import { defineStore } from "pinia";
 
 export const useProductStore = defineStore("productStore", {
 	state: () => ({
 		productByCategory: [],
+		productDetails: null,
 		products: [],
 		error: null,
 		loading: false, // Add an error property to store error information
@@ -33,7 +34,6 @@ export const useProductStore = defineStore("productStore", {
 			} catch (error) {}
 		},
 		async getCategorySpecificProducts(category) {
-			debugger
 			this.loading = true;
 			try {
 				const response = await getProductsByCategory(category);
@@ -45,6 +45,23 @@ export const useProductStore = defineStore("productStore", {
 				}
 			} catch (error) {
 				this.error = "An error occurred while filtering products by category"; // Handle network or other errors
+			} finally {
+				this.loading = false;
+			}
+		},
+		async getProductById(id) {
+			this.loading = true;
+			try {
+				const response = await getProductsById(id);
+				if (response) {
+					// Update a property to store the product details
+					this.productDetails = response.data;
+					this.error = null; // Reset the error if the request was successful
+				} else {
+					this.error = "Server returned a non-200 status code"; // Handle non-200 status codes
+				}
+			} catch (error) {
+				this.error = "An error occurred while fetching product details"; // Handle network or other errors
 			} finally {
 				this.loading = false;
 			}
